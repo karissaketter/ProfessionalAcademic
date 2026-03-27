c87a8206e82b5c72d4e212b00a1d837d86c4db15
wcraske <90268266+wcraske@users.noreply.github.com>
2026-03-27 12:14:40 -0700

    fxi 404 hopefully

--- academic-site/next.config.ts
+++ academic-site/next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/ProfessionalAcademic",
-  assetPrefix: "/ProfessionalAcademic/",
 };
 
 export default nextConfig;
