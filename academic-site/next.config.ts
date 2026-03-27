a42eb5e65fb690a0fbceba009a6f39eeff32cde9
wcraske <90268266+wcraske@users.noreply.github.com>
2026-03-27 12:08:25 -0700

    build for github actions

--- academic-site/next.config.ts
+++ academic-site/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/ProfessionalAcademic",
+  assetPrefix: "/ProfessionalAcademic/",
 };
 
 export default nextConfig;
